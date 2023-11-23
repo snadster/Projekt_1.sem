151bd905bd54d342e2d5dc7d93ed5e4ccb9dee45
Sofie Løfberg <soloe23@student.sdu.dk>
2023-11-23 19:57:08 +0100

    Added a few comments

--- project1/board.py
+++ project1/board.py
@@ -29,7 +29,7 @@ def white(b: Board)-> list[int]:
     [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
     """
     white_indices = []
-    for i in range(0, len(b.board)):
+    for i in range(len(b.board)):
         if b.board[i] == white_piece:
             white_indices.append(i)
     return white_indices
@@ -40,7 +40,7 @@ def black(b: Board)-> list[int]:
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     """
     black_indices = []
-    for i in range(0, len(b.board)):
+    for i in range(len(b.board)):
         if b.board[i] == black_piece:
             black_indices.append(i)
     return black_indices
@@ -76,7 +76,7 @@ def move(m: Move, b: Board) -> None:
             b.board[target(m)] = white_piece
         else:
             b.board[target(m)] = black_piece
-        if not(4 <= abs(source(m) - target(m)) <= 6):   
+        if not(4 <= abs(source(m) - target(m)) <= 6):           #Checks if it is not a nonattack move.
             b.board[(source(m) + target(m))//2] = empty_space
             b.board[source(m)] = empty_space
     b.move = b.move + 1
@@ -89,10 +89,10 @@ def is_legal(m: Move, b: Board) -> bool:
     if (1 <= target(m) <= 25 and b.board[source(m)] != empty_space
         and b.board[target(m)] == empty_space):
         if white_plays(b):
-            if (source(m) % 2 == 1 and row(source(m)) - row(target(m)) == 1
+            if (source(m) % 2 == 1 and row(source(m)) - row(target(m)) == 1     #Only allows pieces on odd numbered squares to move diagonally.
                 and source(m) - target(m) in (6, 5, 4)):
                     legal = True
-            if (source(m)%2==0 and row(source(m)) - row(target(m)) == 1
+            if (source(m) % 2 == 0 and row(source(m)) - row(target(m)) == 1
                 and source(m) - target(m) == 5):
                     legal = True
         else:
@@ -104,7 +104,7 @@ def is_legal(m: Move, b: Board) -> bool:
                     legal = True
         if (row(source(m)) - row(target(m)) in (-2, 0, 2)
             and b.board[(target(m)+source(m))//2] != b.board[source(m)]
-            and b.board[(target(m)+source(m))//2] != empty_space):
+            and b.board[(target(m)+source(m))//2] != empty_space):          #Checks whether the requirements for an attack move is fulfilled.
             if source(m) - target(m) in (-12, -10, - 8, -2, 2, 8, 10, 12):
                 legal = True
     return legal  

134982ea571bdd59e6aac3fba9c22e700dec735d
Sandra K <sjoha23@student.sdu.dk>
2023-11-28 10:34:07 +0100

    minimal text changes

--- project2/alquerque.py
+++ project2/alquerque.py
@@ -3,13 +3,14 @@ from move import *
 
 def start_game() -> None:
     """starts the game"""
-    print("Hear ye hear ye! 'Tis thine ancient game, foretold by the prophecy of yore!"
-          "Thou shalt denounce the wench from which you came, and journey, brave adventurer,"
-          "to our lands of conflict and foes!"
-          "Thou must choose a side, be brave as you go on, this game of Alquerque may last long!"
-          "This quest may become quite fearsome! Upon thy journey the magical machine may ask of you,"
-          "enter upon thine board of keys, a single y for yay, or an n for nay!")
-    comp_white = input("Does thou wish the magical machine to play white? y or n? ").lower()
+    print("Hear ye hear ye!”
+	  "’Tis thine ancient game, foretold by the prophecy of yore!"
+          "Thou shalt journey, brave adventurer, to choose a side;"
+	  "Be brave as you go on, this game of Albuquerque may last long!"
+	  "Thine text may be to small, change this in the Ter-mi-nal.")
+
+    comp_white = (input(“Does thou wish the magical machine to play white?" 
+			"a single y for yay, or an n for nay! ").lower())
     if comp_white == 'y':
         comp_white = True
     else:
@@ -21,9 +22,12 @@ def start_game() -> None:
         comp_black = False
     if (comp_white == False or comp_black == False):
         comp_diff = input("Thine enemy be quick to strike,"
-                          "alas you may decide if his bravery is wondrous,"
-                          "or his pants be yellow! Thine decision may be made upon entering,"
-                          "to your board of keys, a numeral that exists between 0 and 7! ")
+                          "alas you may decide; if"
+			  "his bravery is wondrous?”
+                          "or his pants be yellow!"
+			  "Thine decision may be made upon entering,"
+                          "to your board of keys,"
+			  "a numeral that exists between 0 and 7! ")
 
 def show_board() -> None:
     """Prints a visual representation of the board"""

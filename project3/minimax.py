7010c6d51e45535eb179abafc0ff526595a5c77f
Celine Fussing <celinefussing@Celines-MacBook.local>
2024-01-04 11:48:12 +0100

    added changes to make_tree

--- project3/minimax.py
+++ project3/minimax.py
@@ -13,12 +13,30 @@ class Node:
     
 def make_tree(r: Node, depth: int) -> None:
     """Make a tree with depth chosen by player."""
-    list_of_nodes = [r]
-    while height(r) <= depth:
-        for node in list_of_nodes:
-            for m in legal_moves(node.data):
-                new = add_child(node, m)
-                list_of_nodes.append(new)
+    if depth >= 1:
+        _make_tree(r)
+    elif depth >= 2:
+        for child in r.children:
+            _make_tree(child)
+    elif depth >= 3:
+        for grandchild in r.children.children:
+            _make_tree(grandchild)
+    elif depth >= 4:
+        for grandchild1 in r.children.children.children:
+            _make_tree(grandchild1)
+    elif depth >= 5:
+        for grandchild2 in r.children.children.children.children:
+            _make_tree(grandchild2)
+    elif depth >= 6:
+        for grandchild3 in r.children.children.children.children.children:
+            _make_tree(grandchild3)
+    elif depth >= 7:
+        for grandchild4 in r.children.children.children.children.children.children:
+            _make_tree(grandchild4)
+        
+def _make_tree(n: Node) -> None:
+    for m in legal_moves(n.data):
+        add_child(n, m) 
         
 
 
